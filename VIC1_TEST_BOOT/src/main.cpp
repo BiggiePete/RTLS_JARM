cf4d60e530098592e9bec848bb8e3cecaa986bd1
BiggiePete <pter.cross222@gmail.com>
2025-05-28 22:07:25 -0400

    start working on programming

---
+++ VIC1_TEST_BOOT/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+HardwareSerial Serial_Debug(USART2);
+
+void setup()
+{
+  Serial_Debug.begin(115200);
+  Serial_Debug.println("Hello World");
+}
+unsigned int x = 0;
+void loop()
+{
+  Serial_Debug.printf("Hello %d\n", x++);
+  delay(1000);
+
+  // put your main code here, to run repeatedly:
+}
